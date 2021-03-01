503cae589548be7a404144ab44abe9b0ef12f851
Karim Hakou <karimhako@gmail.com>
2021-03-01 14:31:16 +0100

    readed full csv

--- csv_inlezen.py
+++ csv_inlezen.py
@@ -1,24 +1,27 @@
 import csv
 
 
-reservation_list=[]
+reservationList=[]
+zonesList=[]
 with open('toy1.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file , delimiter=";")
     line_count=0
     for row in csv_reader:
-        if line_count== 0 :
-            print(f'{", ".join(row)}')
-            line_count += 1
+        if row[0][0]=="r":
+            if line_count== 0 :
+                print(f'{", ".join(row)}')
+                line_count += 1
+            else:
+                autos= row[5].split(',')
+                print(f'{row[0]} ,{row[1]} , {row[2]}, {row[3]}, {row[4]}, {autos}, {row[6]}, {row[7]}')
+                reservationList.append(row)
+                line_count += 1
+        elif row[0][0] == "z":
+            zones= row[1].split(',')
+            print(f'{row[0]},{zones}')
         else:
-            auto= row[5].split(',')
-            print(f'{row[0]} ,{row[1]} , {row[2]}, {row[3]}, {row[4]}, {auto}, {row[6]}, {row[7]}')
-            reservation_list.append(row)
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-
-
-print(reservation_list[0][5])
-auto_list=reservation_list[0][5].split(',')
+            print(f'{row}')
+            
+print(reservationList[0][5])
+auto_list=reservationList[0][5].split(',')
 print(auto_list)
-
-
