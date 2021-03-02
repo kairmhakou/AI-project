466964983b80172e947ba3b2594b2987c868a4d5
Jorne Ceulemans <78971352+JorneCeulemans@users.noreply.github.com>
2021-03-02 12:02:47 +0100

    Add files via upload

--- class.py
+++ class.py
@@ -10,8 +10,8 @@ class Reservation:
         self.start = int(day)*1440+int(start) #○convert to minutes (24*60 minutes per day)
         self.end = self.start + int(duration) #cast day, start, duration from txt to int
         
-        self.P1 = P1 # cost for not assigning Reservation
-        self.P2 = P2 # cost for assigning to adjecent zone
+        self.P1 = 7 #P1 # cost for not assigning Reservation
+        self.P2 = 15 #P2 # cost for assigning to adjecent zone
         
         self.x1 = 1 #1 -> not assigned
         self.x2 = 0 #1 -> assigned to adjecent zone
@@ -62,6 +62,46 @@ class Car:
         s += str(self.res)
         return s
     
+class ReservatieLijst:
+    def __init__(self,lijst=None):
+        self.kost=0
+        if lijst==None:
+            self.lijst=[]
+        else:
+            self.lijst=lijst
+        self.l=len(self.lijst)
+        
+    def bereken(self):#Kost berekenen
+        print ('****************kost berekenen **********************')
+        self.kost=0
+        i=0
+        while i<self.l:
+            r=self.lijst[i]
+            self.kost+= r.x1*r.P1 + r.x2*r.P2
+            i+=1
+        print ('De kost is momenteel=', self.kost)
+    
+    def voegtoe(self,a):
+        self.lijst.append(a)
+        self.l+=1
+        
+    def comp(self,a,b):#kijken wat het verschil zou zijn voor een swap
+        print ('****************Vergelijk de kost **********************')
+        kostA=a.x1*a.P1 + a.x2*a.P2
+        kostB=b.x1*b.P1 + b.x2*b.P2
+        verschil= kostB-kostA
+        print ('vershil:', verschil)
+        return verschil
+    
+    def swap(self,a,b):# effectief swappen
+        print ('****************Swappen **********************')
+        i=0
+        while i<self.l:
+            if self.lijst[i]==a:
+                self.lijst[i]=b
+                return
+            i+=1
+            
 reservationList=[]
 zonesList=[]
 carsList=[]
@@ -131,6 +171,11 @@ for res in reservationList:
     reservations.append(r)
     Reservation.resIDtoStr[k] = ID 
     k+=1
+    
+Kost=ReservatieLijst(reservations)
+Kost.bereken()
+Kost.comp(reservations[3],reservations[7])
+Kost.swap(reservations[3],reservations[7])
     
 print('Reservations:')
 for r in reservations:
